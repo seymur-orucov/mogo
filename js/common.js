f1f91f2765e48b3704ecbe791b686b229397e0dc
seymur-orucov <seymur92@hotmail.com>
2018-09-09 20:02:10 +0400

    Final

---
+++ js/common.js
@@ -0,0 +1,76 @@
+﻿
+
+$(document).ready(function() {
+	jQuery('.item').hover(
+		function(){
+			$(this).find(".item-cover").fadeIn();
+		},
+		function(){
+			$(this).find(".item-cover").fadeOut();
+		}
+	);
+	
+	var $container = $(".masonry-container");
+	$container.imagesLoaded(function () {
+		$container.masonry({
+			columnWidth: ".item",
+			itemSelector: ".item"
+		});
+		// $(".item").imagefill();
+	});
+});
+
+$(window).scroll(function () {
+	var aboutOffset = $('.about').offset().top;
+
+	if ($(window).scrollTop() >= aboutOffset) {
+		$('header').addClass('header-add');
+	} else {
+		$('header').removeClass('header-add');
+	}
+});
+
+$("#wrapper").click( function() {
+	$(".menu").toggleClass("closed");
+});
+
+$(function(){
+	$('.main-item').click(function(){
+		$('.burger-menu').toggleClass('burger-opened');
+	});
+});
+
+// Count 
+
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('.count-bars').offset().top - window.innerHeight + 150;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+	$('.count').each(function() {
+	  var $this = $(this),
+		countTo = $this.attr('data-count');
+	  $({
+		countNum: $this.text()
+	  }).animate({
+		  countNum: countTo
+		},
+
+		{
+
+		  duration: 2000,
+		  easing: 'swing',
+		  step: function() {
+			$this.text(Math.floor(this.countNum));
+		  },
+		  complete: function() {
+			$this.text(this.countNum);
+			//alert('finished');
+		  }
+
+		});
+	});
+	a = 1;
+  }
+
+});
